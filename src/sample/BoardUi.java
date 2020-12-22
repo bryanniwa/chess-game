5da55b0b45f652fe0ac7feddf423628c4e1ad4a4
Bryan Niwa <thebryanniwa@gmail.com>
2020-12-21 22:47:51 -0800

    Initial commit

---
+++ src/sample/BoardUi.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class BoardUi extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello world");
+
+        VBox vbox = new VBox(label);
+
+        Scene scene = new Scene(vbox, 100, 100);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
